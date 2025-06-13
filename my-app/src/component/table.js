861cfd4f50e2c6be5815d87ad79c0f9050aec962
rrwnzz <113715978+rrwnzz@users.noreply.github.com>
2025-06-13 20:14:55 +0300

    useState

--- my-app/src/component/table.js
+++ my-app/src/component/table.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import Paper from '@mui/material/Paper';
+import {useState} from 'react' ;
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -37,16 +38,69 @@ const rows = [
 const paginationModel = { page: 0, pageSize: 5 };
 
 export default function DataTable() {
+  const [userData, setUserData] = useState(
+    [
+      { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+      { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+      { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+      { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+      { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+      { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+      { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+      { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+      { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    ]);
+    const [firstName, setFirstName]=useState('');
+    const [lastName, setLastName]=useState('');
+    const [age, setAge]=useState(0);
   return (
     <Paper sx={{ height: 400, width: '100%' }}>
-      <DataGrid
+      {/* <DataGrid }
         rows={rows}
         columns={columns}
         initialState={{ pagination: { paginationModel } }}
         pageSizeOptions={[5, 10]}
         checkboxSelection
         sx={{ border: 0 }}
-      />
+      // {/* /> */}
+      <form onSubmit={(event)=>{
+        event.preventDefault();
+        setUserData([...userData,{id:10, firstName:firstName, lastName:lastName, age:age}])
+      }}>
+        <input onChange={(event)=>{
+          setFirstName(event.target.value);
+        }} type='text' placeholder='enter your first name'></input>
+        <input onChange={(event)=>{
+          setLastName(event.target.value)
+        }} type='text' placeholder='enter your last name'></input>
+        <input onChange={(event)=>{
+          setAge(event.target.value)
+        }} type='number' placeholder='enter your age'></input>
+        <input type='submit'></input>
+      </form>
+      <table border={1}>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>First Name</td>
+            <td>Last Name</td>
+            <td>Age</td>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user,index)=>{
+            return(
+              <tr>
+                <td>{user.id}</td>
+                <td>{user.firstName}</td>
+                <td>{user. lastName}</td>
+                <td>{user.age}</td>
+              </tr>
+
+            )
+          })}
+        </tbody>
+      </table>
     </Paper>
   );
 }
